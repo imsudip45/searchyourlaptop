a25184b8d2da2cc6ac1bd84119c4eab08078662b
Sudip Niroula <161110576+imsudip45@users.noreply.github.com>
2024-11-05 23:28:27 +0545

    update

--- laptop_search/laptop_search/settings.py
+++ laptop_search/laptop_search/settings.py
@@ -118,10 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Add this line if it's not already there
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Media files (User-uploaded files)
 MEDIA_URL = '/media/'
